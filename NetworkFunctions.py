e5a52b3001f91bbb01bc31c6799a6633535dbc9a
rsenne <50930199+rsenne@users.noreply.github.com>
2021-05-03 13:49:26 -0400

    Update NetworkFunctions.py

--- NetworkFunctions.py
+++ NetworkFunctions.py
@@ -8,15 +8,18 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 import scipy.special as sc
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-file = '/home/ryan/The_Ramirez_Group/Cell_Counts.csv'
+file = '/home/ryansenne/The_Ramirez_Group/Cell_Counts.csv'
 
 
 # simple function for loading our csv file
-def loadData(file):
-    data = pd.read_csv(file)
-    return data
+def loadData(data):
+    data = pd.read_csv(data)
+    node_names = data.columns.to_list()
+    node_number = list(item for item in range(0, len(node_names)))
+    nodes = {node_number[i]: node_names[i] for i in range(len(node_number))}
+    return data, nodes
 
 
 # correlate our c-Fos counts between brain regions, df for data
@@ -36,20 +39,160 @@ def corrMatrix(data):
 
 # using this function we will threshold based off of p-values previously calculated
 def significanceCheck(p, corr, alpha):
-    p = np.where((corr >= alpha), 0, p)
+    p = np.where((p >= alpha), 0, p)
     p = np.where((p != 0), 1, p)
     thresholded_matrix = np.multiply(corr, p)
     return thresholded_matrix
 
 
 # we will create our undirected network graphs based on our matrices
-def networx(corr_data):
-    graph = nx.from_numpy_array(corr_data)
-    plt.subplot()
-    nx.draw(graph, with_labels=True, node_color="Blue", node_size=200, edge_color="black", linewidths=2, font_size=15)
-    plt.show()
-    return graph
+def networx(corr_data, nodeLabel):
+    graph = nx.from_numpy_array(corr_data, create_using=nx.Graph)
+    graph = nx.relabel_nodes(graph, nodeLabel)
+    pos = nx.circular_layout(graph)
+    nx.set_node_attributes(graph, pos, name='pos')
+    return graph, pos
 
 
-# calculate the necessary graph attributes such as centrality, betweenness, global efficiency etc.
-def graphAttributes(graph):
+def graphingNetwork(G):
+    global degree_list
+    names = []
+    for x in G.nodes():
+        names.append(x)
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        node_x.append(x)
+        node_y.append(y)
+        degree_list = []
+    for name in names:
+        degree_list.append(G.degree(name))
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers + text',
+        text=names,
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            # colorscale options
+            # 'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+            # 'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+            # 'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=np.array(degree_list) * 15,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right',
+
+            ),
+            line_width=2))
+    node_adjacencies = []
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+    node_trace.marker.color = node_adjacencies
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        title='<br>Mouse Chamber ChR2',
+                        titlefont_size=16,
+                        showlegend=False,
+                        hovermode='closest',
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    fig.update_layout(font_size=40)
+    return fig.show(renderer='browser'), degree_list
+
+
+# # calculate the necessary graph attributes such as centrality, betweenness, global efficiency etc.
+# def graphAttributes(graph, target, threshold, iterations, mode):
+
+
+# this is the disruption propagation model from Vetere et al. 2018
+def disruptPropagate(G, target):
+    G = pd.DataFrame(G)
+    # keep track of negative weights
+    Negs = G
+    Negs = np.where((Negs < 0), -1, Negs)
+    Negs = np.where((Negs > 0), 1, Negs)
+    # keep track of edge updates
+    Update = np.ones(G.shape)
+    # Update = np.ones(G.shape)
+    np.fill_diagonal(Update, 0)
+    # make a copy of the matrix
+    G_0 = abs(G.copy())
+    # remove edges on target node
+    G.loc[target, :] = 0
+    G.loc[:, target] = 0
+    deltaEdge = pd.DataFrame(G_0 - G)
+    Update = np.where((deltaEdge > 0), 0, Update)
+    iterG = [G_0, G]
+    while True:
+        xGraph = iterG[-1]
+        yGraph = deltaEdge.tail(len(deltaEdge))
+        # calculate edge weights
+        deltaEdge_sum = yGraph.sum()
+        edgeWeights = np.add.outer(deltaEdge_sum, deltaEdge_sum)
+        # calculate node weights
+        nodeWeights = xGraph.sum()
+        nodeWeights = np.add.outer(nodeWeights, nodeWeights)
+        nodeWeights = nodeWeights - (2 * xGraph)
+        # print('Hello World')
+        # update edge weights
+        UpdateMult = Update
+        UpdateMult = np.where((Update > 1), 1, UpdateMult)
+        UpdateMult = np.where((Update < 0), 0, UpdateMult)
+
+        deltaEdge_x = edgeWeights / nodeWeights
+        deltaEdge_x.replace(np.nan, 0)
+        deltaEdge_x.replace([np.inf, -np.inf], 0, inplace=True)
+        deltaEdge_x = deltaEdge_x * xGraph * UpdateMult
+
+        # deltaEdge_x.replace([np.inf, -np.inf], 0, inplace=True)
+        # deltaEdge_x.replace(np.nan, 0)
+
+        xGraph = xGraph - deltaEdge_x
+        xGraph = np.where((xGraph < 0), 0, xGraph)
+        # update matrices
+        UpdateSub = Update * 0
+        UpdateSub = np.where((deltaEdge_x > 0), 1, UpdateSub)
+        Update = Update - UpdateSub
+        Update = np.where((xGraph == 0), 0, Update)
+        iterG.append(pd.DataFrame(xGraph))
+        deltaEdge.append(yGraph)
+        if iterG[-1].equals(iterG[-2]):
+            break
+
+    return iterG[-1]
+
+
+# a, b = loadData(file)
+# c, d = corrMatrix(a)
+# e = significanceCheck(d, c, 0.05)
+# q, r = networx(e, b)
+# yo = graphingNetwork(q)
+b = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+disruptPropagate(b, 1)
