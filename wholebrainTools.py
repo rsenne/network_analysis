89ebbc2c299ae6a7598c7fa724c0dfe0510e82c9
rsenne <50930199+rsenne@users.noreply.github.com>
2022-01-19 09:43:23 -0700

    Update wholebrainTools.py

--- wholebrainTools.py
+++ wholebrainTools.py
@@ -4,7 +4,7 @@ import os
 
 def collapseCSVS(filepath):
     files = [filepath + '/' + f for f in os.listdir(filepath) if f.endswith('.csv')]
-    df = pd.concat(map(pd.read_csv, files))
+    df = pd.concat(map(pd.read_csv, files), axis=1)
     df = df.dropna(subset=['name'])
     return df
 
@@ -16,5 +16,12 @@ def getCounts(df):
     return df1
 
 
-df = collapseCSVS('/home/ryansenne/Data/Network/RData/N4_M2')
-counts = getCounts(df)
+def split_and_sum(df):
+    # [s.split(', ')[0] for s in df.T.index.values]
+    d = df.T.groupby([s.split(', ')[0] for s in df.T.index.values]).sum().T
+    return d
+
+
+# df = collapseCSVS('/home/ryansenne/Data/Network/RData/N4_M2')
+# counts = getCounts(df)
+x = collapseCSVS('/home/ryansenne/Desktop/Network_Cells/Final')
