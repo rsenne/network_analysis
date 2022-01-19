1cc5dd70324132980aca75bc334fe25f3b80bdb4
rsenne <50930199+rsenne@users.noreply.github.com>
2022-01-19 09:40:17 -0700

    major changes

--- NetworkFunctions.py
+++ NetworkFunctions.py
@@ -4,27 +4,30 @@ we thank them for their kind support throughout this process"""
 # author:ryan senne/ramirez group
 
 # import necessary libraries
+from numpy.random import random
 import networkx as nx
 import pandas as pd
 import numpy as np
 import scipy.special as sc
-import plotly.graph_objects as go
 import seaborn as sns
 import matplotlib.pyplot as plt
+import markov_clustering as mc
 from bokeh.io import output_file, show
-from bokeh.models import (BoxZoomTool, Circle, HoverTool,
-                          MultiLine, Plot, Range1d, ResetTool,)
+from bokeh.models import (Circle, MultiLine, Plot, Range1d, BoxZoomTool, ResetTool)
 from bokeh.palettes import Spectral4
 from bokeh.plotting import from_networkx
 from bokeh.models import ColumnDataSource, LabelSet
+from IPython import get_ipython
 
 # THIS IS FOR TEST PURPOSES ONLY
-file = 'test.csv'
-
+file = 'TempChR2.csv'
+file2 = 'TempControl.csv'
+get_ipython().run_line_magic('matplotlib', 'qt')
 
 # simple function for loading our csv file
 def loadData(data):
     data = pd.read_csv(data)
+    # data.drop(columns=data.columns[data.nunique() <= 3], inplace=True)
     data = data.apply(lambda x: x.fillna(x.mean()), axis=0)
     node_names = data.columns.to_list()
     node_number = list(item for item in range(0, len(node_names)))
@@ -49,17 +52,19 @@ def corrMatrix(data):
 
 
 # using this function we will threshold based off of p-values previously calculated
-def significanceCheck(p, corr, alpha, threshold=0.0, plot=False):
+def significanceCheck(p, corr, alpha, threshold=0.0, names=None, plot=False, include_Negs=True):
     p = np.where((p >= alpha), 0, p)  # if not significant --> zero
     p = np.where((p != 0), 1, p)  # if significant --> one
+    if not include_Negs:
+        p = np.where((corr < 0), 0, p)
     threshold_matrix = np.multiply(corr, p)  # remove any insignificant correlations
     # remove correlations below threshold
     threshold_matrix = np.where((abs(threshold_matrix) < threshold), 0, threshold_matrix)
     # create a heatmap of correlations if wanted
     if plot:
-        sns.heatmap(threshold_matrix, cmap="coolwarm")
-        plt.show()
-    return threshold_matrix
+        pandas_matrix = pd.DataFrame(threshold_matrix, index=list(names.values()), columns=list(names.values()))
+        sns.clustermap(pandas_matrix)
+    return threshold_matrix, pandas_matrix
 
 
 # we will create our undirected network graphs based on our matrices
@@ -71,102 +76,27 @@ def networx(corr_data, nodeLabel):
     return graph, pos
 
 
-def GraphingNetwork(G):
+def GraphingNetwork(G, plot_title, nx_layout):
     negativeCorr, positiveCorr = 'red', 'black'
     edge_attribs = {}
     for i, j, _ in G.edges(data=True):
         edge_color = negativeCorr if G[i][j]['weight'] < 0 else positiveCorr
         edge_attribs[(i, j)] = edge_color
     nx.set_edge_attributes(G, edge_attribs, "edge_color")
-    plot = Plot(plot_width=900, plot_height=900,
-                x_range=Range1d(-2.1, 2.1), y_range=Range1d(-2.1, 2.1))
-    plot.title.text = "ChR2 Mouse"
-    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
-    pos = graph_renderer.layout_provider.graph_layout
-    x, y = zip(*pos.values())
+    plot = Plot(plot_width=2000, plot_height=2000,
+                x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+    plot.title.text = plot_title
+    plot.add_tools(BoxZoomTool(), ResetTool())
+    graph_renderer = from_networkx(G, nx_layout, scale=1, center=(0, 0))
+    x, y = zip(*graph_renderer.layout_provider.graph_layout.values())
     source = ColumnDataSource({'x': pd.Series(x), 'y': pd.Series(y), 'field': list(G.nodes())})
-    labels = LabelSet(x='x', y='y', text_font_size='10px', x_offset=-11, y_offset=-5, text='field', source=source)
-    graph_renderer.node_renderer.glyph = Circle(size=1, fill_color=Spectral4[0])
-    graph_renderer.edge_renderer.glyph = MultiLine(line_color="edge_color", line_alpha=0.8, line_width=0.1)
+    labels = LabelSet(x='x', y='y', text_font_size='10px', x_offset=0, y_offset=0, text='field', source=source)
+    graph_renderer.node_renderer.glyph = Circle(size=50, fill_color=Spectral4[0])
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="edge_color", line_alpha=0.8, line_width=1)
     plot.renderers.append(graph_renderer)
     plot.renderers.append(labels)
     output_file("interactive_graphs.html")
     show(plot)
-
-
-# this code is taken near-verbatim from the plotly example, I intend to rework the color schemes, but hey, why rebuild
-# the wheel?
-def graphingNetwork(G):
-    global degree_list
-    names = []
-    for x in G.nodes():
-        names.append(x)
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines')
-    node_x = []
-    node_y = []
-    for node in G.nodes():
-        x, y = G.nodes[node]['pos']
-        node_x.append(x)
-        node_y.append(y)
-        degree_list = []
-    for name in names:
-        degree_list.append(G.degree(name))
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers + text',
-        text=names,
-        hoverinfo='text',
-        marker=dict(
-            showscale=True,
-            # colorscale options
-            # 'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-            # 'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-            # 'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-            colorscale='YlGnBu',
-            reversescale=True,
-            color=[],
-            size= np.array(degree_list)*15,
-
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line_width=2))
-    node_adjacencies = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-    node_trace.marker.color = node_adjacencies
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                        title='<br>Mouse Chamber ChR2',
-                        titlefont_size=16,
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=20, l=5, r=5, t=40),
-                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                    )
-
-    fig.update_layout(font_size=40)
-    return fig.show(renderer='browser'), degree_list
-  
   
 # # calculate the necessary graph attributes such as centrality, betweenness, global efficiency etc.
 # def graphAttributes(graph, target, threshold, iterations, mode):
@@ -231,17 +161,47 @@ def disruptPropagate(G, target, nodeNames):
 
 
 # this is all for testing please ignore it
-a, b = loadData(file)
-c, d = corrMatrix(a)
-e = significanceCheck(d, c, 0.05, 0, True)
-q, r = networx(e, b)
-yo = graphingNetwork(q)
-# god = disruptPropagate()
-# dd = [[1, 2, 3],
-#       [4, 5, 6],
-#       [7, 8, 9]]
+# a, b = loadData(file)
+# c, d = corrMatrix(a)
+# e, m = significanceCheck(d, c, 0.0001, 0.0, b, True, True)
+# q, r = networx(e, b)
+# yo = GraphingNetwork(q, "Chr2", nx.circular_layout)
 
-tyty = disruptPropagate(e, "PAG", b)
-mn, nm = networx(tyty.to_numpy(), b)
-graphingNetwork(mn)
+aa, bb = loadData(file2)
+cc, dd = corrMatrix(aa)
+ee, mm = significanceCheck(dd, cc, 0.0001, 0.0, bb, True, True)
+qq, rr = networx(ee, bb)
+# yoyo = GraphingNetwork(qq, "control", nx.circular_layout)
 
+# dxm = e - ee
+numnodes=qq.number_of_nodes()
+positions = {i:(random() * 2 - 1, random() * 2 - 1) for i in range(numnodes)}
+matrix = nx.to_scipy_sparse_matrix(qq)
+result = mc.run_mcl(matrix, inflation=6)           # run MCL with default parameters
+clusters = mc.get_clusters(result)
+# Q = mc.modularity(matrix=result, clusters=clusters)
+# print("inflation:", 20, "modularity:", Q)
+#print("inflation:", inflation, "modularity:", Q)
+# plt.figure()
+# mc.draw_graph(matrix, clusters, pos=positions, node_size=50, with_labels=False, edge_color="silver")
+
+# inflation_values = []
+# for inflation in [i for i in np.linspace(6, 7, 10)]:
+#     result = mc.run_mcl(matrix, inflation=inflation)
+#     clusters = mc.get_clusters(result)
+#     Q = mc.modularity(matrix=result, clusters=clusters)
+#     inflation_values.append(Q)
+#     print("inflation:", inflation, "modularity:", Q)
+
+plt.plot(Q)
+
+
+#   dd = [[1, 2, 3],
+#         [4, 5, 6],
+#         [7, 8, 9]]
+# GraphingNetwork(dd)
+# h = disruptPropagate(dd, 0)
+
+# tyty = disruptPropagate(e, "PAG", b)
+# mn, nm = networx(tyty.to_numpy(), b)
+# # graphingNetwork(mn)
