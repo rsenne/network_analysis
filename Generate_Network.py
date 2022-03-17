2e99b996b0071ea13fcc11c3b76c993faecef1dc
kedorst <kedorst@bu.edu>
2022-03-17 15:50:40 -0400

    Plot for HC results
    -Made a plot to compare both groups and their clustering (could be made into a function in NetworkFunctions.py but I didn't have the bandwidth to do it)

--- Generate_Network.py
+++ Generate_Network.py
@@ -8,8 +8,8 @@ Allen_Groups = list(ROIs.values())
 #ROIs = ROIs.sort_values("Allen Group Name").reset_index(drop = True)
 
 #Then get that data
-ChR2_raw_data,ChR2_nodes = loadData('/Users/kaitlyndorst/Desktop/Data_Analyses/Networks/Network Wrangling/ChR2/ChR2_Large_Network.csv')
-Control_raw_data,Control_nodes = loadData('/Users/kaitlyndorst/Desktop/Data_Analyses/Networks/Network Wrangling/Control/Control_Large_Network.csv')
+ChR2_raw_data,ChR2_nodes = loadData('/Users/kaitlyndorst/Desktop/Network_csv/ChR2_animals/ChR2_Large_Network.csv')
+Control_raw_data,Control_nodes = loadData('/Users/kaitlyndorst/Desktop/Network_csv/Control_animals/Control_Large_Network.csv')
 
 #Get the correlation and adjusted p values for data_ChR2 and data_Control
 ChR2_rVal,ChR2_p_raw,ChR2_p_adj, ChR2_alpha = corrMatrix(ChR2_raw_data)
@@ -21,18 +21,24 @@ ChR2_threshold_matrix,ChR2_pandas_matrix = significanceCheck(ChR2_p_adj, ChR2_rV
 Control_threshold_matrix,Control_pandas_matrix = significanceCheck(Control_p_adj,Control_rVal,alpha = Control_alpha, threshold = 0.001,
                                                                    names=Control_nodes,plot = True,include_Negs = True, Anatomy=ROIs)
 
-#Run some hierarchical clustering
-ChR2_hc_clusters,ChR2_hc_df,ChR2_components = hierarch_clust(ChR2_threshold_matrix,ChR2_nodes,Allen_Groups,plot = False)
-Control_hc_clusters,Control_hc_df,Control_components = hierarch_clust(Control_threshold_matrix,Control_nodes,Allen_Groups,plot = False)
-
 #Using the generated correlation matrices, build a graph using networkx
 ChR2_graph, ChR2_pos = networx(ChR2_threshold_matrix,ChR2_nodes)
 Control_graph, Control_pos = networx(Control_threshold_matrix,Control_nodes)
 
-#Run some markov clustering
-ChR2_markov_df,ChR2_markov_results,ChR2_markov_clusters = markov(ChR2_graph,plot = False)
-plt.savefig("ChR2_markov_graph.jpeg",optimize = True)
+#Run some hierarchical clustering
+ChR2_hc_clusters,ChR2_hc_df= hierarch_clust(ChR2_graph,ChR2_nodes,Allen_Groups,plot = False)
+Control_hc_clusters,Control_hc_df= hierarch_clust(Control_graph,Control_nodes,Allen_Groups,plot = False)
 
-Control_markov_df,Control_markov_results,Control_markov_clusters = markov(Control_graph,plot = False)
-plt.savefig("Control_markov_graph.jpeg",optimize = True)
+#Generate a quick plot for both groups showing cuts along the dendrogram
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.scatter(ChR2_hc_clusters['Distance'],ChR2_hc_clusters['Number of Clusters'],c='b',label='ChR2')
+ax1.scatter(Control_hc_clusters['Distance'],Control_hc_clusters['Number of Clusters'],c='g',label='Control')
+plt.xlabel("Cut Distance Along Dendrogram")
+plt.ylabel("Number of Clusters")
+plt.legend(loc='upper right')
+
+#Run some markov clustering
+ChR2_markov_df,ChR2_markov_clusters = markov(ChR2_graph,plot = False)
+Control_markov_df,Control_markov_clusters = markov(Control_graph,plot = False)
 
