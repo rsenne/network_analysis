df7a4f5e4e82713e1f5bbda18069090a83196ac4
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2023-07-09 21:37:32 -0400

    dsf

--- network_analysis/plotting_utils.py
+++ network_analysis/plotting_utils.py
@@ -50,107 +50,73 @@ def sunflower_r(n, c=1.8):
 
 
 def get_point_cloud(k, c=1.8):
-    n = [i for i in range(1, k + 1)]
-    r = [sunflower_r(i, c) for i in n]
-    theta = [sunflower_theta(j) for j in n]
-    point_cloud_x = (r * np.cos(theta))
-    point_cloud_y = (r * np.sin(theta))
-    point_cloud = [[x, y] for x, y in zip(point_cloud_x, point_cloud_y)]
-    return point_cloud
+    n = np.arange(1, k + 1)
+    r = sunflower_r(n, c)
+    theta = sunflower_theta(n)
+    point_cloud_x = r * np.cos(theta)
+    point_cloud_y = r * np.sin(theta)
+    return np.column_stack((point_cloud_x, point_cloud_y))
 
 
-def generate_circle_centers(num_circles, min_dist, radii):
+def generate_circle_centers(num_circles, min_dist, radii, max_attempts=1000, max_space=250):
     """Generate random (x, y) coordinates for circle centers with given radii"""
-    centers = [(random.uniform(0, 175), random.uniform(0, 175))]
+    space = 150
 
-    while len(centers) < num_circles:
-        # Generate a new random center and radius
-        new_center = (random.uniform(0, 175), random.uniform(0, 175))
-        new_radius = random.choice(radii)
+    while space <= max_space:
+        centers = [(random.uniform(0, space), random.uniform(0, space))]
 
-        # Check if the new circle overlaps with any existing circle
-        too_close = False
-        for center, radius in zip(centers, radii):
-            if math.sqrt((new_center[0] - center[0])**2 + (new_center[1] - center[1])**2) < min_dist + new_radius + radius:
-                too_close = True
-                break
+        attempts = 0
+        while len(centers) < num_circles and attempts < max_attempts:
+            # Generate a new random center
+            new_center = (random.uniform(0, space), random.uniform(0, space))
 
-        # Add the new circle if it doesn't overlap
-        if not too_close:
-            centers.append(new_center)
+            # Check if the new circle overlaps with any existing circle
+            too_close = any(np.hypot(*(np.array(center) - np.array(new_center))) < min_dist + radius
+                            for center, radius in zip(centers, radii))
 
+            # Add the new circle if it doesn't overlap
+            if not too_close:
+                centers.append(new_center)
+            attempts += 1
+
+        if len(centers) == num_circles:
+            return centers  # Successfully placed all circles, return the centers
+
+        print(f"Warning: could only place {len(centers)} out of {num_circles} circles within {max_attempts} attempts. Increasing space.")
+        space += 10  # Increase space by 10 units for the next iteration
+
+    # If we reach here, it means we couldn't place all circles even after increasing the space
+    print(f"Error: could not place all circles even with space={space-10}. Placed only {len(centers)} out of {num_circles}.")
     return centers
-
-def DG_subgraph(cluster_ids, nodes, G, pos_dict, color_list):
-    DG_tuple = [clust for clust in cluster_ids if 24 in clust]
-    DG_cluster = [node for tup in DG_tuple for node in tup]
-    DG_cluster.sort()
-
-    vals = []
-    for node in DG_cluster:
-        vals.append(nodes[node])
-
-    new_color_list = []
-    for node in DG_cluster:
-        new_color_list.append(color_list[node])
-
-    sub_graph = G.subgraph(vals)
-    edge_list = list(sorted(sub_graph.edges()))
-
-    DG_graph = nx.Graph(pos=pos_dict)
-    DG_graph.add_nodes_from(sorted(G.subgraph(vals)))
-    DG_graph.add_edges_from((edge_list))
-
-    deg = DG_graph.degree()
-    node_sizes = [degree / np.mean(list(dict(deg).values())) * 1000 for degree in dict(deg).values()]
-    fig, ax = plt.subplots(figsize=(20, 15))
-    nx.draw_networkx_edges(DG_graph, pos=pos_dict, width=1, edge_color='gainsboro', connectionstyle='arc3,rad=0.2')
-    nx.draw_networkx_nodes(DG_graph, pos=pos_dict, node_size=node_sizes, node_color=new_color_list, linewidths=1,
-                           edgecolors='black')
-    nx.draw_networkx_labels(DG_graph, pos=pos_dict)
-    ax.margins(0.1, 0.05)
-    fig.tight_layout()
-    plt.axis('off')
-    plt.show()
-    return fig
 
 
 def get_position_data(cluster_list, node_names, shape='circular'):
     number_of_clusters = len(cluster_list)
     num_of_nodes = [len(node) for node in cluster_list]
-    pos = generate_circle_centers(number_of_clusters, 50, np.array(num_of_nodes)*(1/20))
-    point_clouds = [get_point_cloud(num_of_nodes[lens], 3) for lens in range(len(num_of_nodes))]
-    for i in range(len(point_clouds)):
-        for j in range(len(point_clouds[i])):
-            point_clouds[i][j][0] += pos[i][0]
-            point_clouds[i][j][1] += pos[i][1]
-    point_cloud_map = {cluster: pos_list for cluster, pos_list in enumerate(point_clouds)}
-    pos_dict = {}
-    for i in range(len(cluster_list)):
-        for j in range(len(cluster_list[i])):
-            pos_dict.update({node_names[cluster_list[i][j]]: np.array(point_cloud_map[i][j])})
-    nx.rescale_layout_dict(pos_dict, 10)
+    pos = generate_circle_centers(number_of_clusters, 30, np.array(num_of_nodes) * (1 / 20))
+    point_clouds = [get_point_cloud(num, 3) + np.array(center) for num, center in zip(num_of_nodes, pos)]
+
+    pos_dict = {node_names[node]: point for idx, cluster in enumerate(cluster_list) for node, point in
+                zip(cluster, point_clouds[idx])}
+
     return pos_dict
 
 
-
-
-
 def graph_network(G, color_list, pos_dict):
+    plt.figure(figsize=(15, 15))
     negativeCorr, positiveCorr = 'lightcoral', 'gainsboro'
-    edge_colors = [negativeCorr if G[i][j]['weight'] < 0 else positiveCorr for i, j, _ in G.edges(data=True)]
-    deg = G.degree()
-    node_sizes = [degree / np.mean(list(dict(deg).values())) * 750 for degree in dict(deg).values()]
-    fig, ax = plt.subplots(figsize=(20, 15))
-    nx.draw_networkx_edges(G, pos=pos_dict, width=1, edge_color=edge_colors, connectionstyle='arc3,rad=0.2')
-    nx.draw_networkx_nodes(G, pos=pos_dict, node_size=node_sizes, node_color=color_list, linewidths=1,
-                           edgecolors='black')
-    nx.draw_networkx_labels(G, pos=pos_dict)
-    ax.margins(0.1, 0.05)
-    fig.tight_layout()
+    edge_colors = [negativeCorr if G[i][j]['weight'] < 0 else positiveCorr for i, j in G.edges]
+    node_sizes = [degree / np.mean(list(dict(G.degree()).values())) * 400 for degree in dict(G.degree()).values()]
+    nx.draw(G, pos=pos_dict,
+            node_color=color_list,
+            node_size=node_sizes,
+            linewidths=1,
+            edge_color=edge_colors,
+            edgecolors='black',
+            with_labels=True)
     plt.axis('off')
+    plt.tight_layout()
     plt.show()
-    return fig
 
 
 def DG_subgraph(cluster_ids, nodes, G, pos_dict, color_list):
